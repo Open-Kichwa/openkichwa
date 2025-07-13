65da476f7d01c3204ef87b4356f5214d0980c967
Ziegenkaese <102874979+Ziegenkaese@users.noreply.github.com>
2025-07-13 20:47:50 +0200

    added a few page bases and new logo

--- src/index.py
+++ src/index.py
@@ -5,3 +5,15 @@ bp = Blueprint("index", __name__)
 @bp.route("/")
 def index():
     return render_template("index.html")
+
+@bp.route("/about")
+def about():
+     return render_template("about.html")
+
+@bp.route("/roadmap")
+def roadmap():
+     return render_template("roadmap.html")
+
+@bp.route("/contribute")
+def contribute():
+     return render_template("contribute.html")
